124f070ed3dd44d5bed90dd326cda59a8c25bfe7
Taneem69 <tanvirtaneem32@gmail.com>
2026-04-12 14:36:10 +0600

    task 3 subtask B completed

---
+++ final_term_lab_1/task3/Task B/form3B.php
@@ -0,0 +1,35 @@
+<?php
+$DOB = "";
+
+if (isset($_POST['DOB'])) {
+    $DOB = $_POST['DOB'];
+    echo "DOB ".$DOB;
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        fieldset{
+            width:100px;
+        }
+    </style>
+    <title>Document</title>
+</head>
+<body>
+    <form action="form3B.php" method="post" enctype="multipart/form-data">
+    <fieldset>
+        <Legend>Name</Legend>
+            Date:<input type="date" name="DOB" value=""><br>
+            <hr>
+            <input type="submit" name="" id="">
+    </fieldset>
+    </form>
+
+</body>
+</html>
