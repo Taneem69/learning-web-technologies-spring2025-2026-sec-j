aeca524486832902aa2d9d23a9dcd19cea3e26a0
Taneem69 <tanvirtaneem32@gmail.com>
2026-04-12 14:52:25 +0600

    task 4 subtask B completed

---
+++ final_term_lab_1/task4/Task B/form4B.php
@@ -0,0 +1,36 @@
+<?php
+$gender="";
+
+if (isset($_POST['gender'])) {
+    $gender = $_POST['gender'];
+    echo "Selected Gender: " . $gender;
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        fieldset{
+            width:150px;
+        }
+    </style>
+    <title>Document</title>
+</head>
+<body>
+    <form action="form4B.php" method="post" enctype="multipart/form-data">
+    <fieldset>
+        <Legend>Gender</Legend>
+            Male<input type="radio" name="gender" value="male"><br>
+            female<input type="radio" name="gender" value="female"><br>
+            Other<input type="radio" name="gender" value="other"><br>
+            <hr>
+            <input type="submit" name="" id="">
+    </fieldset>
+    </form>
+
+</body>
+</html>
