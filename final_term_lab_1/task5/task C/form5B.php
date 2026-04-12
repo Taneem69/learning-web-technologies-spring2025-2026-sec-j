277f41f4b255db5b91ecd0bb620b17de71a816d8
Taneem69 <tanvirtaneem32@gmail.com>
2026-04-12 15:27:43 +0600

    task 5 subtask B completed

---
+++ final_term_lab_1/task5/task C/form5B.php
@@ -0,0 +1,45 @@
+<?php
+
+
+
+if (isset($_POST['degrees'])) {
+    $degrees = $_POST['degrees'];
+
+    echo "Selected Degrees:<br>";
+
+    foreach ($degrees as $d) {
+        echo $d . "<br>";
+    }
+}
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        fieldset{
+            width:100px;
+        }
+    </style>
+    <title>Document</title>
+</head>
+<body>
+    <form action="form5B.php" method="post" enctype="multipart/form-data">
+    <fieldset>
+        <Legend>Degree</Legend>
+        SSC<input type="checkbox" name="degrees[]" value="SSC"><br>
+        HSC<input type="checkbox" name="degrees[]" value="HSC"><br>
+        BSc<input type="checkbox" name="degrees[]" value="BSc"><br>
+        MSc<input type="checkbox" name="degrees[]" value="MSc"><br>
+        <hr>
+        <input type="submit" name="" id="">
+    </fieldset>
+    </form>
+
+</body>
+</html>
