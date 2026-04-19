256416ccb82b31d268dccc16813343d6cc888561
Taneem69 <tanvirtaneem32@gmail.com>
2026-04-19 14:17:24 +0600

    Home page done

---
+++ final_term_lab_2/home.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        h2{
+            display: inline;
+        }
+        header{
+            display: flex;
+            justify-content: space-between;
+            align-item: center;
+
+        }
+
+        div{
+            padding-top:20px;
+        }
+
+        main{
+            height:100vh;
+        }
+
+        footer{
+            text-align:center;
+        }
+    </style>
+    <title>Login Page</title>
+
+</head>
+<body>
+    <header>
+        <h2>XCompany</h4>
+        <div>
+            <a href="">Home</a>|
+            <a href="">Login</a>|
+            <a href="">Registration</a>
+        </div>
+
+    </header>
+    <hr>
+
+    <main>
+        <h1>Welcome to xCompany</h1>
+    </main>
+
+    <hr>
+
+    <footer>
+        <p>Copyright c 2017</p>
+    </footer>
+</body>
+</html>
---
+++ final_term_lab_2/registration.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        h2{
+            display: inline;
+        }
+        header{
+            display: flex;
+            justify-content: space-between;
+            align-item: center;
+
+        }
+
+        div{
+            padding-top:20px;
+        }
+
+        main{
+            height:100vh;
+        }
+
+        footer{
+            text-align:center;
+        }
+    </style>
+    <title>Login Page</title>
+
+</head>
+<body>
+    <header>
+        <h2>XCompany</h4>
+        <div>
+            <a href="">Home</a>|
+            <a href="">Login</a>|
+            <a href="">Registration</a>
+        </div>
+
+    </header>
+    <hr>
+
+    <main>
+        <form action="">
+            <fieldset>
+                <legend>REGISTRATION</legend>
+                Name: <input type="text" name="name"><br>
+                Email: <input type="email" name="email"><br>
+                User Name: <input type="text" name="userName"><br>
+                Password: <input type="password" name="pass"><br>
+                Confirm Password: <input type="password" name:"Cpass"><br>
+                <fieldset>
+                    <legend>Gender</legend>
+                    
+                </fieldset>
+            </fieldset>
+        </form>
+    </main>
+
+    <hr>
+
+    <footer>
+        <p>Copyright c 2017</p>
+    </footer>
+</body>
+</html>
